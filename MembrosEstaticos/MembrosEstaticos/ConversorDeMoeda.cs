e12a3442f32cc44054110822a33ffda86a5501ba
Cleidsan Freire <dsanmike12@gmail.com>
2025-06-01 21:43:27 -0300

    Exercício Membro Estáticos
    Exercício de fixação de membros estáticos com um Conversor de Moedas.

---
+++ MembrosEstaticos/MembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembrosEstaticos
+{
+    internal class ConversorDeMoeda
+    {
+        public static double Iof = 0.06;
+        public static double Conversor(double dolar, double valorComprar)
+        {
+            double total = valorComprar * dolar;
+            return total + total * Iof;
+        }
+    }
+}
---
+++ MembrosEstaticos/MembrosEstaticos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace MembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a Cotação do Dólar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double valorComprar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Valor a ser pago em reais = {ConversorDeMoeda.Conversor(dolar, valorComprar).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
