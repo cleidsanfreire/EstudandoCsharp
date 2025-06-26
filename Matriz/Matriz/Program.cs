56e705cbc163e11043347143bbe82b91a070200b
Cleidsan Freire <dsanmike12@gmail.com>
2025-06-26 15:10:48 -0300

    Matriz

---
+++ Matriz/Matriz/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++) // n linhas
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++) // n colunas 
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("Main diagonal");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nNegative numbers = " + count);
+        }
+    }
+}
