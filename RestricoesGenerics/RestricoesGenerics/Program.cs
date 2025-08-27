f126a8dc383f245766ba6248a5e41a5105c15bd4
Cleidsan Freire <dsanmike12@gmail.com>
2025-08-27 17:08:20 -0300

    Generics

---
+++ RestricoesGenerics/RestricoesGenerics/Program.cs
@@ -0,0 +1,27 @@
+﻿using RestricoesGenerics.Services;
+using System.Numerics;
+
+namespace RestricoesGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                list.Add(number);
+            }
+
+            CalculateService cs = new CalculateService();
+            int max = cs.Max(list);
+            Console.WriteLine("Max:");
+            Console.WriteLine(max);
+        }
+    }
+}
