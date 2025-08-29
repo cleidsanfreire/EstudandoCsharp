1658e7ff7e4ad307ef74bd3a85f70e1e8a2d7b6e
Cleidsan Freire <dsanmike12@gmail.com>
2025-08-28 23:30:05 -0300

    GetHashCode e Equals

---
+++ GetHashCodeTeo/GetHashCodeTeo/Entities/Client.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHashCodeTeo.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+
+        public Client(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public Client()
+        {
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Client))
+            {
+                throw new ArgumentException("This is not a Client");
+            }
+
+            Client other = obj as Client;
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ GetHashCodeTeo/GetHashCodeTeo/Program.cs
@@ -0,0 +1,22 @@
+﻿using GetHashCodeTeo.Entities;
+
+namespace GetHashCodeTeo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+            string f = "(Client) b;";
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+        }
+    }
+}
