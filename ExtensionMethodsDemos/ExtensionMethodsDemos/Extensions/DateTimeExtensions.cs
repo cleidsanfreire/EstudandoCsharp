d66de76fed32d55bca278cd69293c7c9e7552f63
Cleidsan Freire <dsanmike12@gmail.com>
2025-09-05 18:22:39 -0300

    Extension Methods

---
+++ ExtensionMethodsDemos/ExtensionMethodsDemos/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+
+namespace System
+{
+    internal static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan ts = DateTime.Now.Subtract(thisObj);
+
+            if (ts.TotalHours < 24.0)
+            {
+                return ts.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " Hours";
+            }
+            else
+            {
+                return ts.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " Days";
+            }
+        }
+    }
+}
---
+++ ExtensionMethodsDemos/ExtensionMethodsDemos/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ExtensionMethodsDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2025, 01, 01, 00, 00, 00);
+            Console.WriteLine(dt.ElapsedTime());
+
+
+            string st = "Manda pra mim que eu mando pra ele";
+            Console.WriteLine(st.Cut(9));
+
+        }
+    }
+}
